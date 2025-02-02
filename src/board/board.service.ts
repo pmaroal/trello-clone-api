c5e67de33fbd9ef0b0b3eaed79da61414d29e785
PedroMaroal <pedro.maroal@gmail.com>
2025-02-02 20:39:28 +0000

    Create docker configuration

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './entities/board.entity';
+import { Repository } from 'typeorm';
 
 @Injectable() //Indica que esta clase puede ser inyectable en otros lugares
-export class BoardService {}
+export class BoardService {
+  constructor(
+    @InjectRepository(Board)
+    private readonly boardRepository: Repository<Board>,
+  ) {}
+  async getAllBoards(): Promise<Board[]> {
+    return this.boardRepository.find();
+  }
+}
