4eae844b8a21503e023671641b0996292e738ee9
PedroMaroal <pedro.maroal@gmail.com>
2025-02-02 20:57:21 +0000

    Update board files for use bbdd

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { BoardService } from './board.service';
+import { Board } from './entities/board.entity';
 
-@Controller('board')
-export class BoardController {}
+@Controller('board') //Define que este controlador manejará las rutas que comienzan con /boards.
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {} //Usamos el servicio BoardService que ya habíamos creado para obtener los datos de la base de datos.
+
+  @Get() // Este decorador define un método para manejar peticiones HTTP GET. En este caso, cuando se haga una petición a /boards, se llamará a getAllBoards().
+  async getAllBoards(): Promise<Board[]> {
+    //Este método invoca el servicio para obtener todos los boards de la base de datos.
+    return this.boardService.getAllBoards();
+  }
+}
--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from './entities/board.entity';
 
 @Module({
-  controllers: [BoardController],
-  providers: [BoardService]
+  imports: [TypeOrmModule.forFeature([Board])], //Asegura que la entidad Board esté disponible para el repositorio de Board en el servicio.
+  controllers: [BoardController], // Asegura que el BoardController esté registrado en el módulo.
+  providers: [BoardService],
 })
 export class BoardModule {}
