4c5e504da1ee9216626a136bb62e9061b2606733
PedroMaroal <pedro.maroal@gmail.com>
2025-02-03 18:11:41 +0000

    Add function for insert new board in board.service

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -6,10 +6,18 @@ import { Repository } from 'typeorm';
 @Injectable() //Indica que esta clase puede ser inyectable en otros lugares
 export class BoardService {
   constructor(
-    @InjectRepository(Board)
+    @InjectRepository(Board) // Inyectamos el repositorio de Board en el servicio para poder usarlo
     private readonly boardRepository: Repository<Board>,
   ) {}
+
+  // Función para obtener todos los boards de la base de datos
   async getAllBoards(): Promise<Board[]> {
     return this.boardRepository.find();
+  }
+
+  // Función para insertar un nuevo board
+  async createBoard(title: string): Promise<Board> {
+    const newBoard = this.boardRepository.create({ title }); // Usamos el create para crear una nueva instancia
+    return this.boardRepository.save(newBoard); // Guardamos el board en la base de datos
   }
 }
