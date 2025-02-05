12f9f4afaf765329dbcada050301c38f35cb577e
PedroMaroal <pedro.maroal@gmail.com>
2025-02-05 19:37:46 +0000

    update task entity

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -7,14 +7,14 @@ export class Task {
   id: number; // ID único de la tarea
 
   @Column()
-  title: string; // Nombre de la tarea
+  name: string; // Nombre de la tarea
 
   @Column({ nullable: true })
   description?: string; // Descripción opcional
 
-  @Column({ default: 'todo' })
-  status: string; // Estado de la tarea ("todo", "in progress", "done")
+  @Column({ default: false })
+  completed: boolean; // Estado de la tarea
 
   @ManyToOne(() => Board, (board) => board.tasks, { onDelete: 'CASCADE' })
-  board: Board; // La tarea pertenece a un Board
+  board: Board; // Relación: Muchas tareas pertenecen a un Board
 }
